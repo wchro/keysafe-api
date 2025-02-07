4723abfc4eaeb1acb7a8007a84424203f5559442
wchro <93561159+wchro@users.noreply.github.com>
2025-02-07 19:45:12 +0100

    Actualizar esquema de auth para la nueva seguridad

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,6 +4,8 @@ const userSchema = z.object({
   username: z.string().min(2),
   email: z.string().email(),
   password: z.string().min(8),
+  salt: z.string(),
+  masterKey: z.string(),
 });
 
 export default userSchema;
